05be0a27b13bf5aea3ac83e2bea606462c76aa02
lzfelipemonteiro <luizfelippe.p@live.com>
2021-04-12 20:16:24 -0300

    delete labels

---
+++ src/config.py
@@ -0,0 +1,17 @@
+# ------------
+
+NUMERO_AMOSTRAS = 500
+TAXA_DE_SIMBOLO = 50
+NUMERO_DE_SIMBOLO = NUMERO_AMOSTRAS / TAXA_DE_SIMBOLO
+INICIAR_ANIMACAO = False
+
+# ----------- Configuração do gráfico -------------
+MAXIMO_EIXO_Y = 2
+MINIMO_EIXO_Y = -2
+
+MAXIMO_EIXO_DIGITAL_Y = MAXIMO_EIXO_Y
+MINIMO_EIXO_DIGITAL_Y = MINIMO_EIXO_Y
+
+# ------------ Mapeamento de bits -----------------
+VALOR_DE_BITS_PARA_UM = 1
+VALOR_DE_BITS_PARA_ZERO = -1
